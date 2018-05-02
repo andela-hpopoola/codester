3b2b784c50d2ccf657127160997bc1833efc8ca4
Haruna Popoola <haruna.popoola@andela.com>
2018-05-02 13:28:05 +0100

    Add bootstrap styles and pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welcome' });
 });
+
+/* GET About us page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Us' });
+});
+
+/* GET About us page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Us' });
+});
+
 
 module.exports = router;
